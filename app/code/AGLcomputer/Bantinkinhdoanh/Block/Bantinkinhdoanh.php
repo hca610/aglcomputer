b72d5ebd59d74c68ef8cc2beaabe1dad475d8a8e
root <root@ubuntu.bkr4nckf5uquxdlli3kq2aqzza.lx.internal.cloudapp.net>
2021-12-27 23:05:17 +0000

    add Bantinkinhdoanh module

---
+++ app/code/AGLcomputer/Bantinkinhdoanh/Block/Bantinkinhdoanh.php
@@ -0,0 +1,11 @@
+<?php 
+namespace AGLcomputer\Bantinkinhdoanh\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+ 
+class Bantinkinhdoanh extends Template implements BlockInterface {
+
+	protected $_template = "news.phtml";
+
+}
---
+++ app/code/AGLcomputer/Bantinkinhdoanh/registration.php
@@ -0,0 +1,6 @@
+<?php 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'AGLcomputer_Bantinkinhdoanh',
+    __DIR__
+);
