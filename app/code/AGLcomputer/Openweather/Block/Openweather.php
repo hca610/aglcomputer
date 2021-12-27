936a1cd584fba8458f810d44a04874e93ba64ecd
root <root@ubuntu.bkr4nckf5uquxdlli3kq2aqzza.lx.internal.cloudapp.net>
2021-12-27 16:35:28 +0000

    add OpenWeather module

---
+++ app/code/AGLcomputer/Openweather/Block/Openweather.php
@@ -0,0 +1,11 @@
+<?php 
+namespace AGLcomputer\Openweather\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface; 
+ 
+class Openweather extends Template implements BlockInterface {
+
+	protected $_template = "weather.phtml";
+
+}
