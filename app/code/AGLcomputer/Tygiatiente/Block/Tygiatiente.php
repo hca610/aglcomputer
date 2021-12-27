2165063a4db5226fc56de33e3d95b3cb76d8be3a
root <root@ubuntu.bkr4nckf5uquxdlli3kq2aqzza.lx.internal.cloudapp.net>
2021-12-27 19:39:47 +0000

    add Tygiatiente module

---
+++ app/code/AGLcomputer/Tygiatiente/Block/Tygiatiente.php
@@ -0,0 +1,11 @@
+<?php 
+namespace AGLcomputer\Tygiatiente\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+ 
+class Tygiatiente extends Template implements BlockInterface {
+
+	protected $_template = "exchange.phtml";
+
+}
